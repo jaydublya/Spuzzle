16b26f967961dc0229c64a834f694bfed2b1b8a3
William-Crow <crow.william.r@proton.me>
2025-07-29 12:15:27 -0700

    Pause menu script

---
+++ Spuzzle/Assets/Pause Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class PauseMenu : MonoBehavior
+{
+	public GameObject PausePanel;
+	
+	void Update()
+	{
+
+	}
+	public void Pause()
+    {
+		PausePanel.SetActive(true);
+		Time.timeScale = 0;
+    }
+	public void Continue()
+    {
+		PausePanel.SetActive(false);
+		Time.timeScale = 1;
+    }
+}
