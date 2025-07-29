a5136d950ffbeb4e3a9087cdb66fd51fe3e710e7
Timberdoodle42 <dragonix648@gmail.com>
2025-07-29 12:34:43 -0700

    The Title Screen disappears when the Start Button is clicked, and compiler errors in the Pause Menu script have been fixed

--- Spuzzle/Assets/Scripts/GameManager.cs
+++ Spuzzle/Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : MonoBehaviour
     public GameObject titleScreen;
     public bool isGameActive;
     public int itemsCollected;
+    public GameObject pauseMenu;
     
     //This Script will start the game when the Start Button is clicked, end the game when the Quit Button is clicked, will make the game restart when the restart button is clicked, and will make the Game Over screen appear when Gluey is dead
    
@@ -15,6 +16,7 @@ public class GameManager : MonoBehaviour
         isGameActive = false;
         titleScreen.SetActive(true);
         itemsCollected = 0;
+        pauseMenu.SetActive(false);
     }
 
 
