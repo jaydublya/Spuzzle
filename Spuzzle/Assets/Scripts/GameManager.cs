2005701c9d9be8b4a7a121fe1b6da8837dd0d040
Timberdoodle42 <dragonix648@gmail.com>
2025-07-29 12:24:59 -0700

    Added Game Manager as an Empty Object, added StartButton script and GameManager Script

---
+++ Spuzzle/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject titleScreen;
+    public bool isGameActive;
+    public int itemsCollected;
+    
+    //This Script will start the game when the Start Button is clicked, end the game when the Quit Button is clicked, will make the game restart when the restart button is clicked, and will make the Game Over screen appear when Gluey is dead
+   
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        isGameActive = false;
+        titleScreen.SetActive(true);
+        itemsCollected = 0;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGameActive)
+        {
+            titleScreen.SetActive(false);
+        }
+    }
+}
