d5f3eabe3343fcb6025858a1bb5af640f3e9cb76
Timberdoodle42 <dragonix648@gmail.com>
2025-07-30 12:08:17 -0700

    Made the box collider on the enemy a trigger

--- Spuzzle/Assets/Scripts/RotateCamera.cs
+++ Spuzzle/Assets/Scripts/RotateCamera.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 public class RotateCamera : MonoBehaviour
 {
     public float rotationSpeed;
+    private GameManager gameManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+        if (gameManager.isGameActive)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+
+        }
     }
 }
