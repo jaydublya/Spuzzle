324b7ccec5ad4f61a697a11b04d5e868eeecda70
Timberdoodle42 <dragonix648@gmail.com>
2025-07-30 08:37:10 -0700

    Added a "Collectable" List to the ItemIcon Script

--- Spuzzle/Assets/Scripts/ItemIcons.cs
+++ Spuzzle/Assets/Scripts/ItemIcons.cs
@@ -1,8 +1,10 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemIcons : MonoBehaviour
 {
-     
+    public List<GameObject> collectables;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
