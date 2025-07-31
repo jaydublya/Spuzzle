40004a7495a289bea271c34aff9694467081e126
Rennmausmeister <371462@students.bethelsd.org>
2025-07-31 10:23:28 -0700

    Coded the hatch open script

--- Spuzzle/Assets/Scripts/ItemIcons.cs
+++ Spuzzle/Assets/Scripts/ItemIcons.cs
@@ -16,7 +16,11 @@ public class ItemIcons : MonoBehaviour
     public bool wireCollected;
     public bool tapeCollected;
     public bool cogCollected;
-    
+    public float metalVariable;
+    public float batteryVariable;
+    public float wireVariable;
+    public float tapeVariable;
+    public float cogVariable;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -32,18 +36,23 @@ public class ItemIcons : MonoBehaviour
             if (batteryCollected)
             {
                 batteryIcon.SetActive(true);
+                batteryVariable = 1;
             } else if(metalCollected)
             {
                 metalIcon.SetActive(true);
+                metalVariable = metalVariable + 1;
             } else if (wireCollected)
             {
                 wireIcon.SetActive(true);
+                wireVariable = wireVariable + 1;
             } else if (tapeCollected)
             {
                 tapeIcon.SetActive(true);
+                tapeVariable = 1;
             } else if (cogCollected)
             {
                 cogIcon.SetActive(true);
+                cogVariable = 1;
             }
 
 
